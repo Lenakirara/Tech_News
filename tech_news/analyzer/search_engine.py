92311572932456d9b94df23353afaa854a654d4f
Ghislaine <glenox79@gmail.com>
2022-02-11 18:53:43 -0300

    feature: title searches

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import db
+
+
+# case insensitive - mongodb:
+# https://stackoverflow.com/questions/1863399/mongodb-is-it-possible-to-make-a-case-insensitive-query
+# https://docs.mongodb.com/manual/reference/operator/query/regex/
+# https://www.mongodb.com/community/forums/t/case-insensitive-search-with-regex/120598
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # no teste: db.news (...)
+    news = db.news.find({"title": {"$regex": title, "$options": "i"}})
+    news_list = [(post["title"], post["url"]) for post in news]
+    return news_list
 
 
 # Requisito 7
